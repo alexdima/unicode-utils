5c256860b1e4e54b0375165b8451cf7cdabc953a
Alex Dima <alexdima@microsoft.com>
2017-05-05 15:43:19 +0200

    Generate an emoji test regex

--- fetch-data.js
+++ fetch-data.js
@@ -1,26 +1,34 @@
-var VERSION = '9.0.0';
 
 var http = require('http');
 var fs = require('fs');
 
-var options = {
-	hostname: 'www.unicode.org',
-	port: 80,
-	path: `/Public/${VERSION}/ucd/UnicodeData.txt`,
-	method: 'GET'
-};
+function fetchFile(PATH, DEST) {
 
-var req = http.request(options, (res) => {
-	var dest = fs.createWriteStream('./UnicodeData.txt');
+	var options = {
+		hostname: 'www.unicode.org',
+		port: 80,
+		path: PATH,
+		method: 'GET'
+	};
 
-	console.log(`STATUS: ${res.statusCode}`);
-	console.log(`HEADERS: ${JSON.stringify(res.headers)}`);
-	res.pipe(dest);
-});
+	var req = http.request(options, (res) => {
+		var dest = fs.createWriteStream(DEST);
 
-req.on('error', (e) => {
-	console.log(`problem with request: ${e.message}`);
-});
+		console.log(`STATUS: ${res.statusCode}`);
+		console.log(`HEADERS: ${JSON.stringify(res.headers)}`);
+		res.pipe(dest);
+	});
 
-// write data to request body
-req.end();
+	req.on('error', (e) => {
+		console.log(`problem with request: ${e.message}`);
+	});
+
+	// write data to request body
+	req.end();
+}
+
+// fetchFile(`/Public/9.0.0/ucd/UnicodeData.txt`, './UnicodeData.txt');
+// 10.0.0 is still a WIP, but some emojis only show up in 10.0.0
+fetchFile(`/Public/10.0.0/ucd/UnicodeData-10.0.0d5.txt`, './UnicodeData.txt');
+
+fetchFile(`/Public/emoji/5.0/emoji-test.txt`, './emoji-test.txt');
