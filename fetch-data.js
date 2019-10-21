4b9707e52881372b0bae0997868d9ad80739cee3
Alex Dima <alexdima@microsoft.com>
2019-10-21 16:43:34 +0200

    Update data

--- fetch-data.js
+++ fetch-data.js
@@ -27,8 +27,6 @@ function fetchFile(PATH, DEST) {
 	req.end();
 }
 
-// fetchFile(`/Public/9.0.0/ucd/UnicodeData.txt`, './UnicodeData.txt');
-// 10.0.0 is still a WIP, but some emojis only show up in 10.0.0
-fetchFile(`/Public/10.0.0/ucd/UnicodeData-10.0.0d5.txt`, './UnicodeData.txt');
+fetchFile(`/Public/13.0.0/ucd/UnicodeData-13.0.0d6.txt`, './UnicodeData.txt');
 
-fetchFile(`/Public/emoji/5.0/emoji-test.txt`, './emoji-test.txt');
+fetchFile(`/Public/emoji/12.1/emoji-test.txt`, './emoji-test.txt');
