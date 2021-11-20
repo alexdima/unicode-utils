74b00745feb1a984c96a95eb73ea20c1280ba59c
Alex Dima <alexdima@microsoft.com>
2021-11-20 18:19:57 +0100

    Tweak output

--- emoji-test.js
+++ emoji-test.js
@@ -212,6 +212,13 @@ console.log('------');
 console.log(r);
 console.log('------');
 
+fs.writeFileSync('generated/emoji-test.txt', `
+/**
+ * Generated using https://github.com/alexdima/unicode-utils/blob/main/emoji-test.js
+ */
+const CONTAINS_EMOJI = /${r}/;
+`);
+
 // GOTTEN FROM ABOVE RESULT
 var containsEmoji = /(?:[\u231A\u231B\u23F0\u23F3\u2600-\u27BF\u2B50\u2B55]|\uD83C[\uDDE6-\uDDFF\uDF00-\uDFFF]|\uD83D[\uDC00-\uDE4F\uDE80-\uDEFC\uDFE0-\uDFEB]|\uD83E[\uDD00-\uDDFF\uDE70-\uDED6])/;
 
